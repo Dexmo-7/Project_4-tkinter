81db01b4801e20b41d328d32bfa4f4fc9297d8ce
Mateusz Owsianka <mowsianka13@gmail.com>
2021-01-12 21:08:30 +0100

    Some changes.

--- warmup.py
+++ warmup.py
@@ -9,8 +9,17 @@ class Window(Frame):
     def init_window(self):
         self.master.title("GUI")
         self.pack(fill=BOTH, expand=1)
-        quitButton = Button(self, text="Quit", command=self.client_exit)
-        quitButton.place(x=0, y=0)
+
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+        
+        file = Menu(menu)
+        file.add_command(label="Exit", command=self.client_exit)
+        menu.add_cascade(label="File", menu=file)
+        
+        edit = Menu(menu)
+        edit.add_command(label="Undo")
+        menu.add_cascade(label="Edit", menu=edit)
     
     def client_exit(self):
         exit()
